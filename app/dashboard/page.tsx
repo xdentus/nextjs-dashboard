26a7f5af8a2891965cae37b368b7f2b9724498db
xdentus <xdentus@outlook.com>
2024-03-24 19:23:54 +0100

    should work now

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,11 +2,16 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/fonts';
-import { fetchLatestInvoices, fetchCardData } from '@/app/lib/data';
+import {
+  fetchLatestInvoices,
+  fetchCardData,
+  fetchRevenue,
+} from '@/app/lib/data';
 import { Suspense } from 'react';
 import { RevenueChartSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
+  const revenue = await fetchRevenue();
   const latestInvoices = await fetchLatestInvoices();
   const {
     numberOfInvoices,
